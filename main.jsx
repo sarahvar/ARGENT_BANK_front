4e070438c6612c38843a889ae254c392fd33d9ba
sarahvar <sarahvarin95@gmail.com>
2024-04-29 23:30:36 +0200

    Homepage

--- main.jsx
+++ main.jsx
@@ -1,18 +1,16 @@
-// main.jsx
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import store from './src/core/redux/store.js';
-import RouterComponent from './src/routes/router.jsx'; // Assurez-vous d'importer correctement le composant router
+import RouterComponent from './src/routes/router.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <RouterComponent /> {/* Utilisez votre composant router ici */}
+        <RouterComponent />
       </Router>
     </Provider>
   </React.StrictMode>
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,14 +1,22 @@
 import styled from "styled-components";
 import Hero from "./Hero";
 import Features from "./Features";
+import Navbar from "../../layout/Navbar";
+import Footer from "../../layout/Footer";
+
+const HomepageStyled = styled.div`
+  /* Ajoutez vos styles personnalisés ici */
+`;
 
 export default function Homepage() {
   return (
-    <HomepageStyled>
-      <Hero />
-      <Features />
-    </HomepageStyled>
+    <>
+      <Navbar />
+      <HomepageStyled>
+        <Hero />
+        <Features />
+      </HomepageStyled>
+      <Footer />
+    </>
   );
 }
-
-const HomepageStyled = styled.main``;
