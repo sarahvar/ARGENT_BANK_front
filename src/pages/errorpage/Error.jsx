00680c0b27ca1f022c317fa0477bf827c84bb509
sarahvar <sarahvarin95@gmail.com>
2024-05-04 23:21:27 +0200

    errorpage

---
+++ src/pages/errorpage/Error.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import "../errorpage/Error.css";
+
+//Créer la page error en cas d'erreurs et utiliser pour les 3 pages
+const ErrorPage =() => {
+  return (
+    <div className="error-page">
+      <div className="container">
+        <h1 className="title-error">404</h1>
+        <div className="oups">
+          Oups! La page que vous demandez n&apos;existe pas.
+        </div>
+        <Link className="link-accueil" to="/">
+          retourner sur la page d&apos;accueil
+        </Link>
+      </div>
+    </div>
+  );
+}
+export default ErrorPage
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from 'react-router-dom';
 import Homepage from '../pages/homepage/Homepage';
 import Login from '../pages/login/Login';
 import Profile from '../pages/profile/Profile';
+import ErrorPage from '../pages/errorpage/Error';
 
 const RouterComponent = () => (
   <Routes>
     <Route path="/" element={<Homepage />} />
     <Route path="/login" element={<Login />} />
     <Route path="/profile" element={<Profile />} />
+    <Route path="*" element={<ErrorPage />} />
   </Routes>
 );
 
