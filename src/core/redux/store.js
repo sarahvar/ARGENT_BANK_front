b57082a12071e0228ef155106a8180b5de774d48
sarahvar <sarahvarin95@gmail.com>
2024-05-30 18:04:51 +0200

    persist/REHYDRATE

--- src/core/redux/store.js
+++ src/core/redux/store.js
@@ -1,19 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../redux/authSlice'
-import storage from 'redux-persist/lib/storage'
-import { persistReducer } from 'redux-persist'
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../redux/authSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
 const persistConfig = {
     key: 'root',
-    storage, 
-    version: 1
-}
+    storage,
+    version: 1,
+    timeout: 20 * 60 * 1000, // 20 minutes en millisecondes
+};
 
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
-const persistedReducer = persistReducer(persistConfig, authReducer)
-
-export default configureStore({
+const store = configureStore({
     reducer: {
-       auth: persistedReducer 
-    }
-})
+        auth: persistedReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+                ignoredPaths: ['auth.register'],
+            },
+        }),
+});
+
+export const persistor = persistStore(store);
+export default store;
